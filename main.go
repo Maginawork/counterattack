83ca8b0a00ce718f5f63bf6631695c25b4f51059
Maginawork <3_126_wuqiang@sjtu.edu.cn>
2019-09-28 21:49:19 +0800

    modify

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"counterattack/server/cache"
 	"counterattack/server/http"
+	"counterattack/server/tcp"
 )
 
 func main(){
 	c := cache.New("memory")
-
+	go tcp.New(c).Listen()
 	http.New(c).Listen()
 }
